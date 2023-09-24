b75cd74c66d0a3b7e0361d4786f71e70dca832db
Раздольский Владислав <v.razdolskiy@mail.ru>
2023-09-24 23:03:46 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string a;
+Console.WriteLine("Введите день недели");
+a = Convert.ToString(Console.ReadLine());
+switch (a)
+{
+    case "Понедельник":
+    case "понедельник":
+        Console.WriteLine(1); break;
+    case "Вторник":
+    case "вторник":
+        Console.WriteLine(2); break;
+    case "Среда":
+    case "среда":
+        Console.WriteLine(3); break;
+    case "Четверг":
+    case "четверг":
+        Console.WriteLine(4); break;
+    case "Пятница":
+    case "пятница":
+        Console.WriteLine(5); break;
+    case "Суббота":
+    case "суббота":
+        Console.WriteLine(6); break;
+    case "Воскресенье":
+    case "воскресенье":
+        Console.WriteLine(7); break;
+}
+Console.ReadKey();
